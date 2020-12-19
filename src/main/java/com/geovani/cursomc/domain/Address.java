3103fbf930644a1269364f55653df918d5a5f4a2
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-12-19 20:32:31 -0300

    Map Address and Client to database

--- src/main/java/com/geovani/cursomc/domain/Address.java
+++ src/main/java/com/geovani/cursomc/domain/Address.java
@@ -2,8 +2,19 @@ package com.geovani.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Address implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String logradouro;
 	private String number;
@@ -11,8 +22,12 @@ public class Address implements Serializable {
 	private String neighborhood;
 	private String cep;
 	
+	@ManyToOne
+	@JoinColumn(name = "CLIENT_ID")
 	private Client client;
 	
+	@ManyToOne
+	@JoinColumn(name = "CITY_ID")
 	private City city;
 
 	public Address() {}
