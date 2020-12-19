88096269937ca2b0397047fecf453bf6574d1177
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-12-19 19:15:58 -0300

    Create ClientType Enum

---
+++ src/main/java/com/geovani/cursomc/domain/enums/ClientType.java
@@ -0,0 +1,36 @@
+package com.geovani.cursomc.domain.enums;
+
+public enum ClientType {
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURÍDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String description;
+	
+	private ClientType(int cod, String description) {
+		this.cod = cod;
+		this.description = description;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public static ClientType toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(ClientType x : ClientType.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido " + cod);
+	}
+}
