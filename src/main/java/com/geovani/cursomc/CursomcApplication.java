8ba3964baff430f0edf9b97fd070f3900e3bee16
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-12-19 17:23:57 -0300

    Create City and State

--- src/main/java/com/geovani/cursomc/CursomcApplication.java
+++ src/main/java/com/geovani/cursomc/CursomcApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.geovani.cursomc.domain.Category;
+import com.geovani.cursomc.domain.City;
 import com.geovani.cursomc.domain.Product;
+import com.geovani.cursomc.domain.State;
 import com.geovani.cursomc.repositories.CategoryRepository;
+import com.geovani.cursomc.repositories.CityRepository;
 import com.geovani.cursomc.repositories.ProductRepository;
+import com.geovani.cursomc.repositories.StateRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,6 +54,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		State state1 = new State(null, "Minas Gerais");
+		State state2 = new State(null, "São Paulo");
+		
+		City city1 = new City(null, "Uberlândia", state1);
+		City city2 = new City(null, "São Paulo", state2);
+		City city3 = new City(null, "Campinas", state2);
+		
+		state1.getCities().addAll(Arrays.asList(city1));
+		state2.getCities().addAll(Arrays.asList(city2, city3));
+		
+		stateRepository.saveAll(Arrays.asList(state1, state2));
+		cityRepository.saveAll(Arrays.asList(city1, city2, city3));
 	}
 
 }
